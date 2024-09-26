42b1e3a24c84463b424f5a853d16affa72c49c8d
Pacu <150335631+Pacu0101@users.noreply.github.com>
2024-09-26 09:15:01 +0200

    Update Ex1.c

--- Ex1.c
+++ Ex1.c
@@ -1,4 +1,7 @@
 
+/*Progettare un algoritmo che effettui la lettura da tastiera di una serie di coppie di valori numerici.
+L’algoritmo deve calcolare e stampare il rapporto tra il valore minore e quello maggiore dei due. Il
+programma termina quando uno dei due valori o entrambi sono uguali a zero.+/
 #include <stdio.h>
 int main(){
 
